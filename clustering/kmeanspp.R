ed98cf335474620d3cab36d9b44397a52194e377
ethen8181 <ethen8181@gmail.com>
2016-06-07 14:16:32 +0800

    fix major bug in kmeans++

--- clustering/kmeanspp.R
+++ clustering/kmeanspp.R
@@ -1,82 +1,99 @@
-# kmeans++, generating a better k initial random center for kmeans.
-
-# workflow for the kmeans++ algorithm
-# 1. choose a data point at random from the dataset, this serves as the first center point. 
-# 2. compute the distance of all other data points to the randomly chosen center point.
-# 3. to generate the second center point, each data point is chosen with the prob (weight) of 
-#    its distance to the chosen center divided by the the total distance ( in R, sample function's
-#    probability are already weighted, do not need to add up to one ).
-# 4. next recompute the weight of each data point as the minimum of the distance between it and
-#    all the centers that are already generated ( e.g. for the second iteration, compare the 
-#    distance of the data point between the first and second center and choose the smaller one ).
-# 5. repeat step 3 and 4 until having k centers. 
-
-
-# test example data
-
-# remove the species column
-# iris_data <- iris[ , -5 ]
-
-# normalize the dataset 
-# iris_data <- sapply( iris_data, function(x) 
-# {
-#	( x - min(x) ) / ( max(x) - min(x) )
-# })
-
-# ---------------------------------------------------------------------------------
-# [Kmeanspp] :
-# @k = cluster number 
-# returns the kmeans result 
+library(data.table)
 
 Kmeanspp <- function( data, k, ... )
 {
-	if( is.matrix(data) )
-		data <- data.frame(data)
+	# kmeans++, generating a better k initial random center for kmeans. Workflow:
+	# 1. choose a data point at random from the dataset, this serves as the first center point. 
+	# 2. compute the SQUARED distance of all other data points to the randomly chosen center point.
+	# 3. to generate the next center point, each data point is chosen with the prob (weight) of 
+	#    its squared distance to the chosen center of this round divided by the the 
+	#    total squared distance (in R, sample function's probability are already weighted, 
+	#    do not need to tune them to add up to one).
+	# 4. next recompute the weight of each data point as the minimum of the distance between it and
+	#    ALL the centers that are already generated ( e.g. for the second iteration, compare the 
+	#    distance of the data point between the first and second center and choose the smaller one ).
+	# 5. repeat step 3 and 4 until having k centers. 
+	#
+	# Parameters
+	# ----------
+	# data : data.frame, data.table, matrix data
+	#
+	# k : int 
+	#     number of clusters
+	# 
+	# ... : 
+	#     all other parameters that can be passed into R's kmeans except for the data and center
+	#     , see ?kmeans for more detail
+	#
+	# Returns
+	# -------
+	# result : list
+	#     R's kmeans original output
+	#
+	# Reference
+	# ---------
+	# https://datasciencelab.wordpress.com/2014/01/15/improved-seeding-for-clustering-with-k-means/
+
+	if( !is.data.table(data) )
+		data <- data.table(data)
 	
-	# used with bootstrapped data. Avoid duplicates, or kmeans will warn about 
+	# used with bootstrapped data. so unique the data
+	# to avoid duplicates, or kmeans will warn about 
 	# identical cluster center
-	dataset <- unique(data)	
+	unique_data <- unique(data)
+
+	# generate the first center randomly
+	n <- nrow(unique_data)
+	center_ids <- integer(k)
+	center_ids[1] <- sample.int( n, 1 )
+
+	for( i in 1:( k - 1 ) ){		
 		
-	n <- nrow(dataset)	
-	center_ids <- sample.int( n, 1 )
-
-	for( i in 1:( k-1 ) )
-	{		
-		dists <- apply( dataset[ center_ids[i], ], 1, function(center)
-		{
-			sqrt( rowSums( ( dataset - center )^2 ) )
+		# calculate the squared distance between the center and 
+		# all the data points
+		center <- unique_data[ center_ids[i], ]
+		dists <- apply( unique_data, 1, function(datapoint){
+			sum( ( datapoint - center )^2 )
 		})
-	   
-		if( i == 1 )
-		{
+
+		# sample the next center using the squared distance as the weighted probability,
+		# starting from the second center, the measure "squared distance" for each data point
+		# is the min distance between each data point and each center that has already been
+		# generated
+		if( i == 1 ){		
 			distance <- dists
-
-			# somehow distance between the same rows are not zero 
-			# exclude the probability of the choosing the data point that are already chosen as centers
-			dists[center_ids] <- 0
-
-			center_ids[ i+1 ] <- sample.int( n, 1, prob = dists )
-
-		}else
-		{
+		}else{
 			distance <- cbind( distance, dists )
-			
-			probs <- apply( distance, 1, min )			
-			probs[center_ids] <- 0
-			center_ids[ i+1 ] <- sample.int( n, 1, prob = probs )
-		}					
+			distance <- apply( distance, 1, min )
+		}
+		center_ids[ i + 1 ] <- sample.int( n, 1, prob = distance )					
 	}
 
-	# cluster the whole "data", using the center_ids generated from "dataset"
-	results <- kmeans( data, centers = dataset[ center_ids, ], ... )
+	# cluster the whole "data", using the center_ids generated using kmeanspp
+	results <- kmeans( data, centers = unique_data[ center_ids, ], ... )
 	return(results)	
 }
 
-# results <- kmeanspp( data.frame(iris_data) )
 
-# results$size
-# table( iris$Species, results$cluster )
+test <- function(){
+	# test example data
+	# the example code is wrapped in the string below
+	"
+	# remove the species column
+	iris_data <- iris[ , -5 ]
 
-# questionable results on the iris dataset
+	# normalize the dataset
+	iris_data <- data.table( scale(iris_data) )
+	results <- Kmeanspp( data = iris_data, k = 3 )
+
+	# example output, the generated center, size of each cluster
+	# and confusion matrix of the original cluster and clustered result
+	results$center
+	results$size
+	table( iris$Species, results$cluster )
+	"
+	print('testing')
+}
+
 
 
