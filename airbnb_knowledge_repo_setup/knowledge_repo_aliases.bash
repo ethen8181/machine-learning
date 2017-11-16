94b7989281cdcb023a4b54566d561ac8d5e3c962
Eric <ericchang00@gmail.com>
2017-11-16 00:21:07 -0800

    add cli and readme

--- airbnb_knowledge_repo_setup/knowledge_repo_aliases.bash
+++
@@ -1,4 +0,0 @@
-export KNOWLEDGE_HOME="/Users/eric/Documents/machine-learning/knowledge"
-
-alias knowledge_add="knowledge_repo --repo $KNOWLEDGE_HOME add"
-alias knowledge_server="knowledge_repo --repo $KNOWLEDGE_HOME runserver --port 8890"
